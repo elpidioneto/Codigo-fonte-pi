8834a14ff41835749c7ae807762fbca6b67ea081
CAROLINA RIBEIRO <42756880+carolribs@users.noreply.github.com>
2019-06-02 22:51:57 -0300

    Nova pasta de relatório.

---
+++ Relatorios/bancoContasaReceber.sql
@@ -0,0 +1,12 @@
+use relatoriocontasareceber;
+create table lancamentosContasaReceber(
+descricao char(30),
+competencia date,
+valor double, 
+vencimento date,
+data_pagamento double,
+juros_multa double,
+descontos double,
+valor_pago double
+
+);
