c6bb3697ee2fc41270968be6f9e6de404256c27f
Romanini <Romanini@DESKTOP-GCETCHV>
2019-06-10 19:07:49 -0300

    PRocedimentos para o Login de acordo com as informações no BD

--- src/br/iesb/meuprograma/dados/LoginDAO.java
+++ src/br/iesb/meuprograma/dados/LoginDAO.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class LoginDAO implements DAO<Login> {
@@ -94,6 +96,38 @@ while(resultado.next()){
 } catch(SQLException ex) {
     throw new DadosException("Erro ao consultar o Login. Motivo" + ex.getMessage());}
     return lista;   
+    }
+    
+    public boolean checkLogin(String login, String senha) {
+
+        Connection con = ConexaoBD.getConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        boolean check = false;
+
+        try {
+
+            stmt = con.prepareStatement("SELECT * FROM login WHERE login = ? and senha = ?");
+            stmt.setString(1, login);
+            stmt.setString(2, senha);
+
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+
+                
+                check = true;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConexaoBD.closeConnection(con, stmt, rs);
+        }
+
+        return check;
+
     }
  
 }
